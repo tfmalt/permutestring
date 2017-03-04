79d269d355b56ae64e4212ca59b3017cc06b796a
Thomas Malt <thomas@malt.no>
2017-03-04 11:26:04 +0100

    status

--- findword.js
+++ findword.js
@@ -1,39 +1,70 @@
 
 
-const wordlist = [ 'head', 'help', 'hell', 'hall', 'tall', 'tail'];
+const wordlist  = [
+  'wall', 'well', 'head', 'wail', 'help', 'hell', 'hall', 'tall', 'tail',
+  'jail', 'fail', 'hail', 'foil'
+ ];
+const hits      = {};
+const hitstring = {
+  0: '^   ',
+  1: ' ^  ',
+  2: '  ^ ',
+  3: '   ^'
+};
 
 initialState = {
-  candidate:         'wail',
-  answer:            'tail',
+  candidate:         'wall',
   wordlist:          wordlist,
   candidatePosition: 0,
   alphabetPosition:  0,
   foundAnswer:       false
 };
 
-const solution = findword(initialState);
+const solution = findword('sail');
 
 console.log("got solution:", solution);
 
-function findword(state) {
+function findword(candidate, current = '', position = 0, charPos = 0) {
   const letters = 'abcdefghijklmnopqrstuvwxyz';
 
-  if (state.foundAnswer === true || state.candidate === state.answer) {
-    state.foundAnswer = true;
-    console.log("Found answer");
-    return state;
+  let newCandidate = '';
+  let newPosition  = 0;
+  let newCharPos   = 0;
+
+  if(position > 3) {
+    return true;
   }
 
-  let newCandidate = replaceCharcterAt({
-    candPos: candidatePosition, alphPos: alphabetPostion
-  });
-  findword(newCandidate, answer, alphabetPostion++)
+  if (charPos >= letters.length) {
+    console.log('End of alphabet: ', charPos);
+    return false;
+  }
+
+  if (wordlist.includes(candidate) && candidate !== current) {
+    newPosition = position + 1;
+    newCharPos  = 0;
+
+    console.log(' hit:', candidate, current, position)
+    hits[position] = candidate;
+    return findword(candidate, candidate, newPosition, newCharPos);
+  }
+
+  newCandidate = replaceCharacterAt(candidate, position, letters.charAt(charPos));
+  newCharPos   = charPos + 1;
+
+  if (findword(newCandidate, current, position, newCharPos)) {
+    console.log('got true from candidate');
+  } else {
+    newCharPos  = 0;
+    newPosition = position + 1;
+    findword(candidate, current, newPosition, newCharPos);
+  }
 }
 
-function replaceCharacterAt(index, character) {
+function replaceCharacterAt(string, index, character) {
     return (
-      this.substr(0, index) +
+      string.substr(0, index) +
       character +
-      this.substr(index + character.length);
+      string.substr(index + character.length)
     );
 }
