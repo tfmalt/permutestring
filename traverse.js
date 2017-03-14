f301ff9694e8ba6d042176a837879bbc2412a0ad
Thomas Malt <thomas@malt.no>
2017-03-14 15:18:50 +0100

    finising up with user friendly output

--- traverse.js
+++ traverse.js
@@ -1,22 +1,54 @@
+#!/usr/bin/env node
 
+const Walker = require('./lib/word-walker');
+const args   = require('args');
 
-const walker = require('./lib/word-walker');
-
-const wordlist  = [
-  'wall', 'well', 'head', 'wail', 'help', 'hell', 'hall',
-  'tall', 'tail', 'jail', 'fail', 'hail', 'foil', 'heat',
-  'heal', 'sail', 'tell', 'sell'
+// A list of default words to use if no other words are given.
+let words = [
+  'wall', 'well', 'head', 'wail', 'help', 'hell', 'hall', 'tall', 'tail',
+  'jail', 'fail', 'hail', 'foil', 'heat', 'heal', 'sail', 'tell', 'sell'
 ];
 
-const path = [];
+args.option(
+  'words',
+  'A quoted and comma separated list of words you want to test for traversal.'
+);
 
-const start = (list, currpath) => {
-  console.log("Starting iterating over words:");
-  console.log("Word list:", toString(list));
-  let [word] = list.splice(0, 1);
+const options = args.parse(process.argv);
+const errorExit = () => {
+  console.log(
+    'Argument must be a quoted list of comma separated four letter words.'
+  );
+  process.exit(1);
+}
 
-  let newPath = searchList(list, word, currpath);
-  console.log('best path:', toString(newPath));
-};
+if (options.words) {
+  if (typeof options.words !== 'string') {
+    errorExit();
+  }
 
-start(wordlist, path);
+  words = options.words.split(/ *, */);
+  if (words.length < 2) {
+    errorExit();
+  }
+}
+
+console.log("Starting iterating over words:");
+console.log("Word list:", Walker.wordsToString(words));
+console.log('');
+
+
+let ww = new Walker(words);
+
+ww.on('searchlist', (data) => {
+  const pretext = data.list.length ? '    ' : ' !! ';
+  console.log(
+    pretext,
+    Walker.wordsToString(data.path), '    ',
+    Walker.wordsToString(data.list));
+});
+
+let path = ww.findPath();
+
+console.log("");
+console.log('best path:', Walker.wordsToString(ww.bestpath));
